5849b4680c910cd517be831a31f943839e39682e
endrilenne <endrilenne@hotmail.com>
2017-04-10 17:01:07 -0300

    Commit inicial

---
+++ src/main/java/CadastraProdutoBean/CadastraProdutoBean.java
@@ -0,0 +1,20 @@
+package CadastraProdutoBean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.sistemadepedidos.modelo.Produto;
+
+@Named(value = "cadastrarProdutoBean")
+@RequestScoped
+public class CadastraProdutoBean {
+
+    Produto produto = new Produto();
+
+    public void adicionar() {
+        System.out.println(produto);
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+}
---
+++ src/main/java/org/sistemadepedidos/modelo/Produto.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sistemadepedidos.modelo;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private int quantidade;
+    private double preco;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", categoria=" + categoria + ", quantidade=" + quantidade + ", preco=" + preco + '}';
+    }
+    
+    
+} 
